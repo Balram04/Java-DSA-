360d785f54780973d7357204572471066f7e3bde
Balram Prajapati <0105it221045@oriental.ac.in>
2026-02-26 19:42:31 +0530

    Add MaxSumSubArray.java to implement a function that calculates the maximum sum of a contiguous subarray

---
+++ MaxSumSubArray.java
@@ -0,0 +1,19 @@
+public class MaxSumSubArray {
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 3, 4, -1, 2, 1, -5, 4};
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int x : arr) {
+            currentSum += x;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        System.out.println("Maximum sum of a contiguous subarray is: " + maxSum);
+    }
+}
