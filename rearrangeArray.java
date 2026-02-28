f783ec3e1a4c57aff25417d6f0854a2388fb5e45
Balram Prajapati <0105it221045@oriental.ac.in>
2026-02-28 13:59:42 +0530

    Add RearrangeArray.java to implement a function that rearranges an array by alternating positive and negative numbers

---
+++ rearrangeArray.java
@@ -0,0 +1,27 @@
+class RearrangeArray {
+    public static int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int pos = 0; // Pointer for even indices
+        int neg = 1;  // Pointer for odd indices
+
+        for (int num : nums) {
+            if (num > 0) { // Positive number
+                result[pos] = num;
+                pos += 2; // Move to the next even index
+            } else { // Negative number
+                result[neg] = num;
+                neg += 2; // Move to the next odd index
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, -2, 1, -5, 4, -6};
+        int[] rearranged = rearrangeArray(nums);
+        for (int num : rearranged) {
+            System.out.print(num + " ");
+        }
+    }
+}
